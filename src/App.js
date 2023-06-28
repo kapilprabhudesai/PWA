4a34cf100cbcaa40b899986b50a045efe12854cf
RavirajParab <ravirajparab44@gmail.com>
2023-06-28 17:32:29 +0530

    bg sync working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Axios from 'axios';
+
 
 function App() {
+
+  const postTasks = async () => {
+    const res = await Axios.post('http://localhost:4000/api/tasks', {
+      "id": 123,
+      "info": "Some task"
+    })
+    console.log(res)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={postTasks}>POST</button>
     </div>
   );
 }
--- src/custom-serviceWorker.js
+++ src/custom-serviceWorker.js
@@ -1,4 +1,8 @@
 import { precacheAndRoute } from 'workbox-precaching';
+import { NetworkOnly } from 'workbox-strategies';
+import { BackgroundSyncPlugin } from 'workbox-background-sync';
+import {registerRoute} from 'workbox-routing';
+
 // eslint-disable-next-line no-undef
 // if (workbox) {
 //     console.log(`Workbox is loaded 🎉`);
@@ -7,6 +11,17 @@ import { precacheAndRoute } from 'workbox-precaching';
 // }
 // eslint-disable-next-line no-undef, no-restricted-globals
 precacheAndRoute(self.__WB_MANIFEST);
+const bgSyncPlugin = new BackgroundSyncPlugin('myPendingTasks', {
+    maxRetentionTime: 24 * 60, // Retry for max of 24 Hours (specified in minutes)
+});
+
+registerRoute(
+    'http://localhost:4000/api/tasks',
+    new NetworkOnly({
+        plugins: [bgSyncPlugin],
+    }),
+    'POST'
+);
 // eslint-disable-next-line
 // self.addEventListener('install', event => event.waitUntil(self.skipWaiting()));
 // // eslint-disable-next-line
